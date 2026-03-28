5e34f2b9c614691d588daab5da98550e5feadb73
loveyantraweb <loveyantra.web@gmail.com>
2026-03-28 16:59:29 +0530

    Add files via upload

---
+++ teachers.js
@@ -0,0 +1,33 @@
+const teachers = {
+
+t1: {
+  name: "Ikra Khan",
+  photo: "images/ikra.jpeg"
+},
+
+t2: {
+  name: "Ayush",
+  photo: "images/ayush.jpg"
+},
+
+t3: {
+  name: "Bharti",
+  photo: "images/bharti.jpg"
+},
+
+t4: {
+  name: "Dr Sharma",
+  photo: "images/sharma.jpg"
+},
+
+t5: {
+  name: "Prof Gupta",
+  photo: "images/gupta.jpg"
+},
+
+t6: {
+  name: "HOD Verma",
+  photo: "images/verma.jpg"
+}
+
+};
